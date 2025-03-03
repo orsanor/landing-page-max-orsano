10b399e80242300a979966d984ae7cdd0f37e7fd
Jonathan Orsano <orsanor@gmail.com>
2025-03-03 19:22:24 -0300

    fix: footer

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -4,11 +4,16 @@ export function Footer() {
   return (
     <section className="bg-[#183981] py-16 text-white">
       <div className="container mx-auto px-4">
-        <footer className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols3 gap-8 mb-12 mt-5">
+        <footer className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mb-12 mt-5">
           <div>
             <h3 className="text-2xl font-semibold mb-4">Contatos</h3>
             <p>Email: max.orsano@gmail.com</p>
             <p>Celular: (61) 99999-9999</p>
+          </div>
+
+          <div>
+            <h3 className="text-2xl font-semibold mb-4">Onde Encontrar</h3>
+            <h1>Aguas Claras</h1>
           </div>
 
           <div>
