e504c9c9ff3c46712ee920724af51da1027eb12d
Jonathan Orsano <orsanor@gmail.com>
2025-03-05 20:34:49 -0300

    ajuste de responsividade

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -5,16 +5,24 @@ import { Link } from "react-scroll";
 
 export function Header() {
   return (
-    <header className="px-6 fixed top-0 left-0 w-full z-50 bg-white">
-      <div className="max-w-6xl mx-auto flex items-center justify-between">
-        <nav>
-          <ul className="flex gap-6 font-semibold">
+    <header className="px-4 fixed top-0 left-0 w-full z-50 bg-white shadow-md">
+      <div className="max-w-6xl mx-auto flex items-center justify-between flex-wrap md:flex-nowrap">
+        <Link
+          to="hero"
+          smooth={true}
+          duration={500}
+          className="mr-auto md:mr-0"
+        >
+          <img src="/logo.png" className="h-20 w-auto cursor-pointer" />{" "}
+        </Link>
+        <nav className="w-full md:w-auto mt-4 md:mt-0">
+          <ul className="flex flex-col md:flex-row gap-4 md:gap-6 font-semibold">
             <li>
               <Link
                 to="hero"
                 smooth={true}
                 duration={500}
-                className="text-black hover:text-[#0D68B1] duration-300 font-semibold cursor-pointer"
+                className="text-black hover:text-[#0D68B1] duration-300 cursor-pointer"
               >
                 Inicio
               </Link>
@@ -24,7 +32,7 @@ export function Header() {
                 to="about"
                 smooth={true}
                 duration={500}
-                className="text-black hover:text-[#0D68B1] duration-300 font-semibold cursor-pointer"
+                className="text-black hover:text-[#0D68B1] duration-300 cursor-pointer"
               >
                 Sobre
               </Link>
@@ -34,7 +42,7 @@ export function Header() {
                 to="services"
                 smooth={true}
                 duration={500}
-                className="text-black hover:text-[#0D68B1] duration-300 font-semibold cursor-pointer"
+                className="text-black hover:text-[#0D68B1] duration-300 cursor-pointer"
               >
                 Serviços
               </Link>
@@ -44,7 +52,7 @@ export function Header() {
                 to="contact"
                 smooth={true}
                 duration={500}
-                className="text-black hover:text-[#0D68B1] duration-300 font-semibold cursor-pointer"
+                className="text-black hover:text-[#0D68B1] duration-300 cursor-pointer"
               >
                 Contato
               </Link>
@@ -54,22 +62,19 @@ export function Header() {
                 to="blog"
                 smooth={true}
                 duration={500}
-                className="text-black hover:text-[#0D68B1] duration-300 font-semibold cursor-pointer"
+                className="text-black hover:text-[#0D68B1] duration-300 cursor-pointer"
               >
                 Blog
               </Link>
             </li>
           </ul>
         </nav>
-        <Link to="hero" smooth={true} duration={500}>
-          <img src="/logo.png" className="h-32 mr-24 cursor-pointer" />
-        </Link>
         <a
           href={`https://wa.me/556198275031?text=Gostaria de agendar uma sessão`}
-          className="bg-[#0D68B1] px-6 py-3 rounded-md font-semibold flex items-center w-fit gap-4 text-white hover:bg-[#77adeb] duration-300"
+          className="bg-[#0D68B1] px-4 py-2 rounded-md font-semibold flex items-center w-fit gap-2 text-white hover:bg-[#77adeb] duration-300 mt-4 md:mt-0" // ajuste de padding e gap
         >
           <WhatsappLogo className="w-5 h-5" />
-          Agende sua sessão
+          Agende
         </a>
       </div>
     </header>
